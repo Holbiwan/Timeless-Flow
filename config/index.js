7bafe7f6c814ebfb61e6799f49eac14ec516138e
Holbiwan <sabrinapapeau@gmail.com>
2025-01-14 20:37:14 +0100

    Handle index file in config dir

--- config/index.js
+++ config/index.js
@@ -1,5 +1,15 @@
+// Centralized config exports
+// This file consolidates all configurations for easy imports elsewhere in the app
+
+// Visual assets
 import { Images } from "./images";
 import { Colors } from "./theme";
+
+// Firebase services
 import { auth } from "./firebase";
 
-export { Images, Colors, auth };
+export {
+  Images, // App images (logos, icons, etc.)
+  Colors, // Theme colors for consistent styling
+  auth,   // Firebase authentication instance
+};
