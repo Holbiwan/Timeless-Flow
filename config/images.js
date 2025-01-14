2ac5ff4a3845a43f11327c076ebfd1d56a22fc1a
Holbiwan <sabrinapapeau@gmail.com>
2025-01-14 20:33:16 +0100

    images.js

--- config/images.js
+++ config/images.js
@@ -1,3 +1,7 @@
+// Centralized image management for the application
 export const Images = {
-  logo: require("../assets/TimelessLogo.png"),
+  logo: require("../assets/TimelessLogo.png"), // Main logo for the app
+  // Add other images below as needed
+  splash: require("../assets/splash.png"), // Splash screen image
+  icon: require("../assets/icon.png"), // App icon
 };
