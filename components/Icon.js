d14d53daf85b828bf00faea153ea85f1f85de4c0
Holbiwan <sabrinapapeau@gmail.com>
2025-01-14 19:45:07 +0100

    Handle Icon component

--- components/Icon.js
+++ components/Icon.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export const Icon = ({ name, size, color, style }) => {
+/**
+ * Icon Component
+ * A wrapper around MaterialCommunityIcons to easily use icons in the app.
+ * @param {string} name - The name of the icon (from MaterialCommunityIcons).
+ * @param {number} size - The size of the icon.
+ * @param {string} color - The color of the icon.
+ * @param {object} style - Additional styles for the icon.
+ */
+export const Icon = ({ name, size = 24, color = '#000', style = {} }) => {
   return (
     <MaterialCommunityIcons
       name={name}
@@ -10,4 +19,19 @@ export const Icon = ({ name, size, color, style }) => {
       style={style}
     />
   );
+};
+
+// PropTypes for better validation
+Icon.propTypes = {
+  name: PropTypes.string.isRequired, // Name is required because it's essential for the icon
+  size: PropTypes.number, // Size of the icon (default is 24)
+  color: PropTypes.string, // Color of the icon (default is black)
+  style: PropTypes.object, // Custom styles for the icon
+};
+
+// Default props
+Icon.defaultProps = {
+  size: 24,
+  color: '#000',
+  style: {},
 };
