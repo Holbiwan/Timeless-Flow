9cb3f777299043c4fc59f65cf87d774c2ee92de0
Holbiwan <sabrinapapeau@gmail.com>
2025-01-14 20:12:28 +0100

    Handle View Component

--- components/View.js
+++ components/View.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View as RNView, StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-export const View = ({ isSafe, style, children }) => {
+/**
+ * View Component
+ * A wrapper for the native View component, with optional safe area handling.
+ * @param {boolean} isSafe - Whether to respect safe area insets.
+ * @param {object} style - Custom styles for the container.
+ * @param {React.Node} children - Child elements to render inside the view.
+ */
+export const View = ({ isSafe = false, style = {}, children }) => {
   const insets = useSafeAreaInsets();
 
-  if (isSafe) {
-    return (
-      <RNView style={{ paddingTop: insets.top, ...style }}>{children}</RNView>
-    );
-  }
+  const safeStyle = isSafe
+    ? {
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+      }
+    : {};
 
-  return <RNView style={StyleSheet.flatten(style)}>{children}</RNView>;
+  return <RNView style={StyleSheet.compose(safeStyle, style)}>{children}</RNView>;
 };
+
+// PropTypes for validation
+View.propTypes = {
+  isSafe: PropTypes.bool, // Indicates if the view should respect safe area insets
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]), // Custom styles
+  children: PropTypes.node, // Child elements to render inside the View
+};
+
+// Default props
+View.defaultProps = {
+  isSafe: false,
+  style: {},
+};
+
+const styles = StyleSheet.create({});
