d2ea46fc8d4abde12212ff60ba99953e8cb51938
Holbiwan <sabrinapapeau@gmail.com>
2025-01-20 05:53:06 +0100

    Push modifications

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
-import { RootNavigator } from "./navigation/RootNavigator";
+import { ThemeProvider } from "react-native-elements"; // Import ThemeProvider
 import { AuthenticatedUserProvider } from "./providers";
+import { RootNavigator } from "./navigation/RootNavigator";
+
+// Optionally, define a custom theme
+const theme = {
+  colors: {
+    primary: "#4CAF50", // Example: green as primary color
+    secondary: "#FF9800", // Example: orange as secondary color
+  },
+  Button: {
+    raised: true, // All buttons will be raised by default
+  },
+};
 
 const App = () => {
   return (
-    <AuthenticatedUserProvider>
-      <SafeAreaProvider>
-        <RootNavigator />
-      </SafeAreaProvider>
-    </AuthenticatedUserProvider>
+    <ThemeProvider theme={theme}>
+      <AuthenticatedUserProvider>
+        <SafeAreaProvider>
+          <RootNavigator />
+        </SafeAreaProvider>
+      </AuthenticatedUserProvider>
+    </ThemeProvider>
   );
 };
 
