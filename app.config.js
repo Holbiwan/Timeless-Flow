fe55e5a103799588be129b8f085f3ed6bd91ecbd
Holbiwan <sabrinapapeau@gmail.com>
2025-01-20 03:50:52 +0100

    update expo slug

--- app.config.js
+++ app.config.js
@@ -2,8 +2,8 @@ import "dotenv/config";
 
 export default {
   expo: {
-    name: "Expo Firebase Starter",
-    slug: "expo-firebase",
+    name: "Timeless-Flow",
+    slug: "timeless-flow",
     privacy: "public",
     platforms: ["ios", "android"],
     version: "0.19.0",
@@ -28,6 +28,10 @@ export default {
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
       appId: process.env.APP_ID,
+      eas: {
+        projectId: "eb2a64e6-f793-4c6a-8745-cdfb1708377c",
+      },
     },
+    owner: "holbiwan971",
   },
 };
