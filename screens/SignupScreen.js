94e5c304b8dbeeabffe86f0e3f752e444a97896e
Holbiwan <sabrinapapeau@gmail.com>
2025-01-24 07:01:28 +0100

    Reuse InputField & Button from components ans simplify handlers

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -4,126 +4,63 @@ import { Formik } from "formik";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-import { View, TextInput, Logo, Button, FormErrorMessage } from "../src/components";
-import { Images, Colors, auth } from "../config";
-import { useTogglePasswordVisibility } from "../src/hooks";
+import { View, InputField, Logo, Button, FormErrorMessage } from "../components";
+import { Colors, auth } from "../config";
 import { signupValidationSchema } from "../utils";
-import { InputField, Button } from '../components';
-
+import { useTogglePasswordVisibility } from "../hooks";
 
 export const SignupScreen = ({ navigation }) => {
   const [errorState, setErrorState] = useState("");
+  const { passwordVisibility, handlePasswordVisibility, rightIcon } = useTogglePasswordVisibility();
 
-  const {
-    passwordVisibility,
-    handlePasswordVisibility,
-    rightIcon,
-    handleConfirmPasswordVisibility,
-    confirmPasswordIcon,
-    confirmPasswordVisibility,
-  } = useTogglePasswordVisibility();
-
-  const handleSignup = async (values) => {
+  const handleSignup = (values) => {
     const { email, password } = values;
-
-    createUserWithEmailAndPassword(auth, email, password).catch((error) =>
-      setErrorState(error.message)
-    );
+    createUserWithEmailAndPassword(auth, email, password).catch((error) => setErrorState(error.message));
   };
 
   return (
-    <View isSafe style={styles.container}>
-      <KeyboardAwareScrollView enableOnAndroid={true}>
-        {/* LogoContainer: consist app logo and screen title */}
+    <View style={styles.container}>
+      <KeyboardAwareScrollView enableOnAndroid>
         <View style={styles.logoContainer}>
-          <Logo uri={Images.logo} />
+          <Logo />
           <Text style={styles.screenTitle}>Create a new account!</Text>
         </View>
-        {/* Formik Wrapper */}
         <Formik
-          initialValues={{
-            email: "",
-            password: "",
-            confirmPassword: "",
-          }}
+          initialValues={{ email: "", password: "" }}
           validationSchema={signupValidationSchema}
-          onSubmit={(values) => handleSignup(values)}
+          onSubmit={handleSignup}
         >
-          {({
-            values,
-            touched,
-            errors,
-            handleChange,
-            handleSubmit,
-            handleBlur,
-          }) => (
+          {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
             <>
-              {/* Input fields */}
-              <TextInput
+              <InputField
                 name="email"
-                leftIconName="email"
                 placeholder="Enter email"
                 autoCapitalize="none"
                 keyboardType="email-address"
-                textContentType="emailAddress"
-                autoFocus={true}
                 value={values.email}
                 onChangeText={handleChange("email")}
                 onBlur={handleBlur("email")}
               />
               <FormErrorMessage error={errors.email} visible={touched.email} />
-              <TextInput
+              <InputField
                 name="password"
-                leftIconName="key-variant"
                 placeholder="Enter password"
                 autoCapitalize="none"
-                autoCorrect={false}
                 secureTextEntry={passwordVisibility}
-                textContentType="newPassword"
-                rightIcon={rightIcon}
-                handlePasswordVisibility={handlePasswordVisibility}
                 value={values.password}
                 onChangeText={handleChange("password")}
                 onBlur={handleBlur("password")}
+                rightIcon={rightIcon}
+                handlePasswordVisibility={handlePasswordVisibility}
               />
-              <FormErrorMessage
-                error={errors.password}
-                visible={touched.password}
-              />
-              <TextInput
-                name="confirmPassword"
-                leftIconName="key-variant"
-                placeholder="Enter password"
-                autoCapitalize="none"
-                autoCorrect={false}
-                secureTextEntry={confirmPasswordVisibility}
-                textContentType="password"
-                rightIcon={confirmPasswordIcon}
-                handlePasswordVisibility={handleConfirmPasswordVisibility}
-                value={values.confirmPassword}
-                onChangeText={handleChange("confirmPassword")}
-                onBlur={handleBlur("confirmPassword")}
-              />
-              <FormErrorMessage
-                error={errors.confirmPassword}
-                visible={touched.confirmPassword}
-              />
-              {/* Display Screen Error Messages */}
-              {errorState !== "" ? (
-                <FormErrorMessage error={errorState} visible={true} />
-              ) : null}
-              {/* Signup button */}
-              <Button style={styles.button} onPress={handleSubmit}>
-                <Text style={styles.buttonText}>Signup</Text>
-              </Button>
+              <FormErrorMessage error={errors.password} visible={touched.password} />
+              <Button style={styles.button} onPress={handleSubmit} title="Signup" />
             </>
           )}
         </Formik>
-        {/* Button to navigate to Login screen */}
         <Button
-          style={styles.borderlessButtonContainer}
           borderless
-          title={"Already have an account?"}
+          title="Already have an account?"
           onPress={() => navigation.navigate("Login")}
         />
       </KeyboardAwareScrollView>
@@ -132,37 +69,8 @@ export const SignupScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Colors.white,
-    paddingHorizontal: 12,
-  },
-  logoContainer: {
-    alignItems: "center",
-  },
-  screenTitle: {
-    fontSize: 32,
-    fontWeight: "700",
-    color: Colors.black,
-    paddingTop: 20,
-  },
-  button: {
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 8,
-    backgroundColor: Colors.orange,
-    padding: 10,
-    borderRadius: 8,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: Colors.white,
-    fontWeight: "700",
-  },
-  borderlessButtonContainer: {
-    marginTop: 16,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  container: { flex: 1, backgroundColor: Colors.black, padding: 16 },
+  logoContainer: { alignItems: "center" },
+  screenTitle: { fontSize: 24, fontWeight: "bold", color: Colors.white, marginVertical: 16 },
+  button: { marginVertical: 16, backgroundColor: Colors.primary },
 });
