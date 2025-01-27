458764ceed99875a735dfb15855a585c8b886cf9
Holbiwan <sabrinapapeau@gmail.com>
2025-01-27 10:12:15 +0100

    Add an index file to handle exports

---
+++ screens/index.js
@@ -0,0 +1,5 @@
+export { LoginScreen } from "./LoginScreen";
+export { SignupScreen } from "./SignupScreen";
+export { ForgotPasswordScreen } from "./ForgotPasswordScreen";
+export { HomeScreen } from "./HomeScreen";
+export { FeedbackScreen } from "./FeedbackScreen";
